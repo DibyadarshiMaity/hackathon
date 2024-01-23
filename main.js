d28d9f956d56db943a80064963e997b755109ac1
Dibyadarshi Maity <137876930+DibyadarshiMaity@users.noreply.github.com>
2024-01-24 00:18:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var typed=new Typed('.content',{
+    strings:['Welcome to new era of gaming'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+    }); 
+
+// let sections = document.querySelectorAll('section');
+
+// window.onscroll = ()=>{
+//     sections.forEach(sec =>{
+//     let top =window.scrollY;
+//     let offset = sec.offsetTop;
+//     let height =sec.offsetHeight;
+    
+//     if(top>=offset && top < offset+height){
+//         sec.classList.add('show-animate');
+//     }
+//     else{
+//         sec.classList.remove('show-animate');
+//     }
+// })
+// }
